7cf8879dfddaeda1bdc14b9718162be172cb9d2e
Tom Huybrechts <tom@trantor.(none)>
2010-01-30 15:54:29 +0100

    compile fixes

--- maven-plugin/src/main/java/hudson/maven/MavenModuleSet.java
+++ maven-plugin/src/main/java/hudson/maven/MavenModuleSet.java
@@ -759,7 +759,6 @@ public final class MavenModuleSet extends AbstractMavenProject<MavenModuleSet,Ma
         return FormValidation.ok();
     }
 
-    @Override
     public DescriptorImpl getDescriptor() {
         return DESCRIPTOR;
     }
